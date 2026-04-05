a18bf3d0c0c2969575436ea3c0b0334e9bd579a1
Mutudu <aeondxf@live.com>
2026-04-05 20:55:16 +0800

    Add PicoLog metrics surface
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/PicoLog/GlobalUsings.cs
+++ src/PicoLog/GlobalUsings.cs
@@ -3,6 +3,7 @@
 global using System.Collections.Concurrent;
 global using System.Collections.Immutable;
 global using System.Diagnostics;
+global using System.Diagnostics.Metrics;
 global using System.Text;
 global using System.Threading.Channels;
 global using PicoLog.Abs;
