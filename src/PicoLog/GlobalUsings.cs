642bab31e7f8f3f39bbd8ef8263795f4460d8b05
Mutudu <aeondxf@live.com>
2026-04-19 09:37:24 +0800

    Move compiler services import to global usings
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/PicoLog/GlobalUsings.cs
+++ src/PicoLog/GlobalUsings.cs
@@ -4,6 +4,7 @@ global using System.Collections.Concurrent;
 global using System.Collections.Immutable;
 global using System.Diagnostics;
 global using System.Diagnostics.Metrics;
+global using System.Runtime.CompilerServices;
 global using System.Text;
 global using System.Threading.Channels;
 global using PicoLog.Abs;
